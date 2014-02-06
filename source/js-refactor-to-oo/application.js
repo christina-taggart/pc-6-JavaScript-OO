11451db2c0b92d66183c60958518a95772d64a33
Alexander Leishman <null>
2014-02-05 23:03:34 -0800

    Refactored code using OO principles

--- source/js-refactor-to-oo/application.js
+++ source/js-refactor-to-oo/application.js
@@ -1,13 +1,57 @@
-$(document).ready(function() {
-  $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
-  });
+// On document load...
+$(function(){
 
-  $('#roller button.roll').on('click', function() {
+  var game = new Game;  //initialize game
+
+// Listen for events
+  function eventListeners(){
+
+    // Listen for add button click
+    $('#roller button.add').click(function(){
+      game.addDie(); // Add a die to the screen
+    })
+    // Listen for roll click
+    $('#roller button.roll').click(function(){
+      game.rollDice(); // Roll the dice
+    })
+  }
+
+//####### Define Die model ##########
+
+// Define Die constructor
+  function Die(){
+    this.value = this.roll(); //set random value on initialization
+  }
+
+// Roll a die and return roll value
+  Die.prototype.roll = function(){
+    return this.value = Math.floor((Math.random()*6)+1);
+  }
+
+// Generate the html for a die
+  Die.prototype.genHtml = function(){
+    return '<div class="die">' + this.value + '</div>';
+  }
+
+// ###### Define game model ######
+
+// Create game "class"
+  function Game(){
+  }
+
+// Add die to the screen
+  Game.prototype.addDie = function(){
+    $('.dice').append(new Die().genHtml());
+  }
+
+// Roll all of the die on the scree
+  Game.prototype.rollDice = function(){
     $('.die').each(function(k, die) {
-      var value = Math.floor((Math.random()*6)+1);
-      $(die).text(value);
+      $(die).text(new Die().roll());
     });
-  });
+  }
+
+// Initialize even listeners function
+  eventListeners();
+
 });
