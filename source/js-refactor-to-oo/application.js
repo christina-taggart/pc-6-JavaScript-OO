39e384f9b021ef5bd0d693217704a4d4dd0d0260
Patrick <Patrick@Patricks-MacBook-Air.local>
2014-02-06 21:19:17 -0800

    Refactored

--- source/js-refactor-to-oo/application.js
+++ source/js-refactor-to-oo/application.js
@@ -1,13 +1,37 @@
 $(document).ready(function() {
-  $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
-  });
-
-  $('#roller button.roll').on('click', function() {
-    $('.die').each(function(k, die) {
-      var value = Math.floor((Math.random()*6)+1);
-      $(die).text(value);
-    });
-  });
+  
+  var board = new Board;
+  eventListeners(board);
+  
 });
+
+function eventListeners(board){
+    $('#roller button.roll').on('click', function() {
+      board.rollDice();
+    });
+
+    $('#roller button.add').on('click', function() {
+      board.addDie();
+    })
+  }
+
+function Die() {
+  this.val = 0
+}
+
+Die.prototype.roll = function(){
+  return this.val = Math.floor((Math.random()*6)+1);
+}
+
+function Board(){}
+
+Board.prototype.rollDice = function(){
+  $('.die').each(function(k, die) {
+    $(die).text(new Die().roll())
+  })
+}
+
+Board.prototype.addDie = function(){
+  var die = new Die
+  $('.dice').append("<div class='die'>"+die.val+"</div>");
+}
