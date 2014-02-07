13c66b9003d716c16cf28e89d18eae94396038ab
Spencer Smitherman <spencer.smitherman@gmail.com>
2014-02-06 21:35:08 -0800

    Added OO elements to procedural Javascript

--- source/js-refactor-to-oo/application.js
+++ source/js-refactor-to-oo/application.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
-  $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
+
+  var newDie = '<div class="die">0</div>'
+
+  var rollDie = function(k, die){
+    $(die).text(Math.floor((Math.random()*6)+1))
+  };
+
+  $('.add').click(function() {
+    $('.dice').append(newDie);
   });
 
-  $('#roller button.roll').on('click', function() {
-    $('.die').each(function(k, die) {
-      var value = Math.floor((Math.random()*6)+1);
-      $(die).text(value);
-    });
+  $('.roll').click(function() {
+    $('.die').each(rollDie)
   });
+
 });
