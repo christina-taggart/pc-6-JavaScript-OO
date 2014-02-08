388cf2929c16b09651f325269538d3008a0b3f70
Emmanuel Kaunitz <ekaunitz@gmail.com>
2014-02-07 21:34:38 -0800

    refactored app using object literals to extract methods from event handlers.

--- source/js-refactor-to-oo/application.js
+++ source/js-refactor-to-oo/application.js
@@ -1,13 +1,24 @@
 $(document).ready(function() {
   $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
+    appendDiv()
   });
 
   $('#roller button.roll').on('click', function() {
-    $('.die').each(function(k, die) {
-      var value = Math.floor((Math.random()*6)+1);
-      $(die).text(value);
-    });
+    rollDice()
   });
 });
+
+
+function randomNumber() {
+  return Math.floor((Math.random()*6)+1);
+}
+
+function appendDiv() {
+  $('.dice').append('<div class="die">0</div>');
+}
+
+function rollDice() {
+  $('.die').each(function(k, die) {
+      $(die).text(randomNumber);
+    });
+}
