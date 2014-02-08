0fb0fb0df34bd71738cc57f4e61ce74653ccead7
mh120888 <matthew.alan.higgins@gmail.com>
2014-02-07 22:05:00 -0800

    First OO refactor of dice rolling 'game'

--- source/js-refactor-to-oo/application.js
+++ source/js-refactor-to-oo/application.js
@@ -1,13 +1,42 @@
+// EVENTS
+
 $(document).ready(function() {
+  var newGame = new Game;
   $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
+    newGame.addDie();
   });
 
   $('#roller button.roll').on('click', function() {
-    $('.die').each(function(k, die) {
-      var value = Math.floor((Math.random()*6)+1);
-      $(die).text(value);
-    });
-  });
+    $('.die').remove();
+    newGame.rollDice();
+  });  
 });
+
+// DIE
+
+function Die() {
+  this.value = 0;
+}
+
+Die.prototype.roll = function() {
+  this.value = Math.floor((Math.random()*6)+1);
+}
+
+// GAME
+
+function Game() {
+  this.dice = [];
+}
+
+Game.prototype.addDie = function() {
+  var newDie = new Die;
+  this.dice.push(new Die);
+  $('.dice').append('<div class="die">' + newDie.value +  '</div>');
+}
+
+Game.prototype.rollDice = function() {
+  for (i=0; i<this.dice.length; i++) {
+    this.dice[i].roll();
+    $('.dice').append('<div class="die">' + this.dice[i].value +  '</div>');
+  }
+}
