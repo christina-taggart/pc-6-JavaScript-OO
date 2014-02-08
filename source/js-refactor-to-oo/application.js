4cc61b0dd5628a58680dbe87c0481523dfbf6132
Ricardo Rubio <rickarubio@gmail.com>
2014-02-07 20:57:10 -0800

    refactored js code to be more OO

--- source/js-refactor-to-oo/application.js
+++ source/js-refactor-to-oo/application.js
@@ -1,13 +1,23 @@
 $(document).ready(function() {
+
+  var Die = function() {
+    var roll = function() { return Math.floor((Math.random()*6)+1); }
+    var markup = '<div class="die">0</div>';
+    var all_dice = $('.die');
+    this.append = function() { $('.dice').append(markup); };
+    this.roll_all_dice = function() { 
+      $.each(all_dice, function(idx, die) {
+        $(die).text(roll());
+      }); 
+    }
+  };
+
   $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
+    new Die().append();
   });
 
   $('#roller button.roll').on('click', function() {
-    $('.die').each(function(k, die) {
-      var value = Math.floor((Math.random()*6)+1);
-      $(die).text(value);
-    });
+    new Die().roll_all_dice();
   });
+
 });
