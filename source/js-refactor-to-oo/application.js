21f0aa334aa4c0dd8dbcc01987d74a086cb322ef
Christina Taggart <ctaggart@Christinas-MacBook-Pro-2.local>
2014-02-09 15:23:18 -0800

    Release 0: Refactor JavaScript and added amazing dice colors

--- source/js-refactor-to-oo/application.js
+++ source/js-refactor-to-oo/application.js
@@ -1,13 +1,32 @@
 $(document).ready(function() {
-  $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
-  });
 
-  $('#roller button.roll').on('click', function() {
-    $('.die').each(function(k, die) {
+  var die = {
+    roll: function(index, die) {
       var value = Math.floor((Math.random()*6)+1);
       $(die).text(value);
-    });
-  });
+    },
+    node: '<div class="die">0</div>',
+    makeRandomColor: function(index, die){
+      var colors = ["blue", "red", "green", "yellow"];
+      var randomColor = colors[ Math.floor( Math.random() * colors.length ) ];
+      $(die).css( 'background-color', randomColor );
+    }
+  }
+
+  var game = {
+    rollAll: function() {
+      $('.die').each( die.roll )
+      //here's a function, execute when you're ready...callback
+    },
+    add: function(){
+      $('.dice').append( die.node );
+    },
+    colorize: function(){
+      $('.die').each( die.makeRandomColor );
+    }
+  }
+
+  $('#roller button.add').on('click', game.add)
+  $('#roller button.roll').on('click', game.rollAll)
+  $('#roller button.colorize').on('click', game.colorize)
 });
