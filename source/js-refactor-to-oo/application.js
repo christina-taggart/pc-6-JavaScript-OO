adbe4a98097de61fc1ab71492155b4f5650e0e19
hzla00 <neohzla@gmail.com>
2014-02-09 01:13:03 -0800

    refactored code into object orientation

--- source/js-refactor-to-oo/application.js
+++ source/js-refactor-to-oo/application.js
@@ -1,13 +1,24 @@
 $(document).ready(function() {
+  
+  function die(){
+    this.add = add;
+    this.roll = roll
+    function add(){
+      $('.dice').append('<div class="die">0</div>')
+    }
+    function roll(){
+      $('.die').each(function(k, die) {
+        var value = Math.floor((Math.random()*6)+1);
+        $(die).text(value)
+      });
+    };
+  };
+  
+  var new_die = new die
   $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
+    new_die.add()
   });
-
   $('#roller button.roll').on('click', function() {
-    $('.die').each(function(k, die) {
-      var value = Math.floor((Math.random()*6)+1);
-      $(die).text(value);
-    });
+    new_die.roll()
   });
 });
