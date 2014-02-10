f347616f76f20248d6ed2ab646b880a0c4e99e12
Elliot Chalfant <epchalfant@gmail.com>
2014-02-09 22:53:31 -0800

    more OO now

--- source/js-refactor-to-oo/application.js
+++ source/js-refactor-to-oo/application.js
@@ -1,13 +1,43 @@
+
+
 $(document).ready(function() {
+  roller = new Roller
   $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
+    roller.add();
   });
 
   $('#roller button.roll').on('click', function() {
-    $('.die').each(function(k, die) {
-      var value = Math.floor((Math.random()*6)+1);
-      $(die).text(value);
-    });
+    roller.rollAll();
   });
 });
+
+// $(document).ready(function() {
+//   roller = new Dice
+//   $('#roller button.add').on('click', roller.add)
+//   $('#roller button.roll').on('click', roller.roll)
+// });
+
+function Roller() {
+}
+
+function Die() {
+  this.roll = Math.floor((Math.random()*6)+1);
+}
+
+Roller.prototype.rollAll = function() {
+  $('.die').each(function(k, die) {
+  $(die).text(new Die().roll);
+  });
+}
+
+Roller.prototype.add = function() {
+  $('.dice').append('<div class="die">0</div>');
+}
+
+
+
+
+// Provide good names for all anonymous functions.
+// Move the anonymous functions to objects.
+// Each callback or event handler is only 1 or 2 lines
+// Create a clear separation of concerns.
