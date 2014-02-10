16fe5ecf5367e3b08bf3d572d7bb799008164b64
Apprentice <apprentice@dbc15.local>
2014-02-09 21:30:17 -0800

    Refactored

--- source/js-refactor-to-oo/application.js
+++ source/js-refactor-to-oo/application.js
@@ -1,13 +1,29 @@
 $(document).ready(function() {
-  $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
-  });
 
-  $('#roller button.roll').on('click', function() {
-    $('.die').each(function(k, die) {
-      var value = Math.floor((Math.random()*6)+1);
-      $(die).text(value);
-    });
-  });
+  function dieRollerClass() {
+    var that = this;
+
+    that.generate = function() {
+      return Math.floor((Math.random()*6)+1);
+    };
+
+    that.add = function() {
+      $('.dice').append('<div class="die">' + that.generate() + '</div>');
+    };
+
+    that.rollDice = function() {
+      $('.die').each(that.changeSides)
+    };
+
+    that.changeSides = function(i, die) {
+      $(die).text(that.generate());
+    };
+
+  };
+
+  var dieRoller = new dieRollerClass();
+
+  $('#roller button.add').on('click', dieRoller.add);
+  $('#roller button.roll').on('click', dieRoller.rollDice);
+
 });
