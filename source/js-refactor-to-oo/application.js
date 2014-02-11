1fd8904b69ba252729fc40cbf67bc96aea62450b
K Y <kevin@kevinyeh.com>
2014-02-10 23:41:49 -0800

    Refactor add dice and roll dice

--- source/js-refactor-to-oo/application.js
+++ source/js-refactor-to-oo/application.js
@@ -1,13 +1,65 @@
 $(document).ready(function() {
+  $myDiceArea = $('.dice')
+  myDiceBox = new DiceBox()
+  myDiceView = new DiceBoxView(myDiceBox)
+
   $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
+    myDiceBox.addDie()
+    $myDiceArea.empty()
+    $myDiceArea.append(myDiceView.renderDiceBox())
   });
 
   $('#roller button.roll').on('click', function() {
-    $('.die').each(function(k, die) {
-      var value = Math.floor((Math.random()*6)+1);
-      $(die).text(value);
-    });
+    myDiceBox.rollAll()
+    $myDiceArea.empty()
+    $myDiceArea.append(myDiceView.renderDiceBox())
   });
 });
+
+function DiceBox() {
+  this.dice= []
+}
+
+DiceBox.prototype = {
+  addDie: function() {
+    this.dice.push(new Die())
+  },
+  rollAll: function() {
+    for(var i=0; i<this.dice.length; i++) {
+      this.dice[i].roll()
+    }
+  }
+}
+
+function Die() {
+  this.faces= [1,2,3,4,5,6]
+  this.current_face= 1
+}
+
+Die.prototype = {
+  roll: function() {
+    this.current_face = this.faces[Math.floor(Math.random()*this.faces.length)]
+  }
+}
+
+function DiceBoxView(diceBox) {
+  this.getView = ""
+  this.diceBox = diceBox
+}
+
+DiceBoxView.prototype = {
+  renderDiceBox: function() {
+    newView = ""
+    for(var i = 0; i < this.diceBox.dice.length ; i++) {
+      newView += getViewElement(this.diceBox.dice[i].current_face)
+    }
+    return newView
+  }
+}
+
+function getViewElement(dieValue) {
+  $dieTemplate = $('#die_template').clone()
+  console.log($dieTemplate.html())
+  $dieTemplate.find('div').text(dieValue)
+  return $.trim($dieTemplate.html())
+}
