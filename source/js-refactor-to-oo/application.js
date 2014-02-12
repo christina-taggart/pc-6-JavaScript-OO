ee145f7892ad2800ca0737e8534b3f92782598ea
Ricardo Rubio <rickarubio@gmail.com>
2014-02-11 19:12:42 -0800

    refactored taking @sgharms comments into consideration

--- source/js-refactor-to-oo/application.js
+++ source/js-refactor-to-oo/application.js
@@ -1,23 +1,38 @@
 $(document).ready(function() {
 
-  var Die = function() {
-    var roll = function() { return Math.floor((Math.random()*6)+1); }
-    var markup = '<div class="die">0</div>';
-    var all_dice = $('.die');
-    this.append = function() { $('.dice').append(markup); };
-    this.roll_all_dice = function() { 
-      $.each(all_dice, function(idx, die) {
-        $(die).text(roll());
-      }); 
-    }
+  var myDice = new DiceContainer();
+
+  function DiceContainer() {
+    this.dice = [];
+  }
+
+  DiceContainer.prototype.roll_all_dice = function() {
+    var currentDiceContainer = this;
+    var diceNodes = $('.die');
+    $.each(diceNodes, function(idx, dieNode) {
+      $(dieNode).text(currentDiceContainer.dice[idx].roll());
+    }); 
   };
 
+  function Die() {
+    this.dieMarkup = '<div class="die">0</div>';
+  };
+
+  Die.prototype.append = function() { 
+    $('.dice').append(this.dieMarkup); 
+  };
+
+  Die.prototype.roll = function() { 
+    return Math.floor((Math.random()*6)+1); 
+  }
+
   $('#roller button.add').on('click', function() {
-    new Die().append();
+    myDice.dice.push(new Die());
+    myDice.dice[myDice.dice.length -1].append();
   });
 
   $('#roller button.roll').on('click', function() {
-    new Die().roll_all_dice();
+    myDice.roll_all_dice();
   });
 
 });
