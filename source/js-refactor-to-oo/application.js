3bdfff89059653cf0943967082eca3575a2651c7
Emmanuel Kaunitz <ekaunitz@gmail.com>
2014-02-11 23:24:49 -0800

    refactored javascript to folow OO principles, its super awesome now thanks to help from RAO

--- source/js-refactor-to-oo/application.js
+++ source/js-refactor-to-oo/application.js
@@ -1,24 +1,46 @@
 $(document).ready(function() {
   $('#roller button.add').on('click', function() {
-    appendDiv()
+  theController.createDie()
   });
 
   $('#roller button.roll').on('click', function() {
-    rollDice()
+    theController.rollAllDice()
   });
 });
 
 
-function randomNumber() {
+
+function Die() {
+  this.template = '<div class="die">0</div>'
+}
+
+Die.prototype.appendYoself = function() {
+  $('.dice').append(this.template)
+}
+
+Die.prototype.setHTMLdie = function() {
+  this.$htmlDie = $('.die').last()
+}
+
+Die.prototype.random = function() {
   return Math.floor((Math.random()*6)+1);
 }
 
-function appendDiv() {
-  $('.dice').append('<div class="die">0</div>');
+Die.prototype.rollDie = function () {
+  this.$htmlDie.text(this.random())
 }
 
-function rollDice() {
-  $('.die').each(function(k, die) {
-      $(die).text(randomNumber);
-    });
+var theController = {
+  createDie: function () {
+    var die = new Die()
+    die.appendYoself()
+    die.setHTMLdie()
+    this.diceRack.push( die ) 
+  },
+  diceRack: [],
+  rollAllDice: function () {
+    for (var i=0; i<this.diceRack.length; i++) {
+      this.diceRack[i].rollDie()
+    }
+  }
 }
